da42ff237971445a184e9b5ec5fbcdff17b7de2d
pauladelavictoria <pauladelavictoria@users.noreply.github.com>
2026-08-10 09:52:54 +0200

    improve cache

--- src/libs/queryClient.ts
+++ src/libs/queryClient.ts
@@ -1,3 +1,11 @@
 import { QueryClient } from "@tanstack/react-query";
 
-export const queryClient = new QueryClient();
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 5 * 60_000,
+      gcTime: 10 * 60_000,
+      retry: 1,
+    },
+  },
+});
